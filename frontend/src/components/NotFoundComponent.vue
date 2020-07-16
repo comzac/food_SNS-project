beb3a4340f7b346eeed7d38fe6205107d45d1246
YongjoonSeo <koreakkrea12@naver.com>
2020-07-16 15:13:31 +0900

    NotFoundComponent revised & frontend-skeleton deleted

--- frontend/src/components/NotFoundComponent.vue
+++ frontend/src/components/NotFoundComponent.vue
@@ -1,11 +1,19 @@
 <template>
   <h1 class="text-center mt-10">
       Page Not Found
+      <div class="my-2">
+        <v-btn small @click="moveToHome">홈으로</v-btn>
+      </div>
   </h1>
 </template>
 
 <script>
 export default {
+    methods: {
+        moveToHome() {
+            this.$router.push({ name: 'Home' })
+        }
+    }
 
 }
 </script>
