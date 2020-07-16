8d2bb4f5babaa14f79d2b85e227b707d2dc24e5c
rsm4282@naver.com <rsm4282@naver.com>
2020-07-16 13:27:07 +0900

    Merge vuex & css

---
+++ frontend/src/views/LoginView3.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="login">
+      <h1 class="text-center">로그인</h1>
+      <div class="login-form">
+        <div class="form-group">
+            <input class="form-control" @keypress.enter="login(loginData)" v-model="loginData.uid" id="uid" type="text" placeholder="아이디">
+        </div>
+        <div class="form-group">
+            <input class="form-control" @keypress.enter="login(loginData)" v-model="loginData.upw" id="upw" type="password" placeholder="비밀번호">
+        </div>
+        <div class="d-flex justify-content-between">
+            <button class="btn btn-primary" @click="login(loginData)">로그인</button>
+            <button class="btn btn-success" @click="moveToSignup">회원가입</button>
+        </div>
+      </div>
+  </div>
+
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+// import store from "@/store/index"
+
+export default {
+    name: 'LoginView',
+    data() {
+        return {
+            loginData: {
+                uid: null,
+                upw: null,
+            }
+        }
+    },
+    methods: {
+        ...mapActions(['login']),
+        moveToSignup() {
+            this.$router.push({ name: 'Signup' })
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
