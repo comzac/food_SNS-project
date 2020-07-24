cd4d72b98509d7cae178d44af3fb8a36d7b7f4af
rsm4282@naver.com <rsm4282@naver.com>
2020-07-24 18:54:34 +0900

    Feat password check

--- frontend/src/store/accounts.js
+++ frontend/src/store/accounts.js
@@ -189,6 +189,31 @@ export default {
         })
         .catch((err) => console.log(err.response));
     },
+
+    pwcheck({ state, rootGetters }, password) {
+      const config = rootGetters["accounts/config"];
+      const requestData = {
+        "uid": state.userData.uid,
+        "upw": password
+      }
+      return axios
+        .post(
+          SERVER.BASE_URL + 
+            SERVER.ROUTES.accounts.URL + 
+            SERVER.ROUTES.accounts.pwcheck,
+          requestData,
+          config
+        )
+        .then(res => {
+          if(res.data === "success") {
+            return true
+          } else {
+            return false
+          }
+        })
+        .catch(err => console.log(err))
+    },
+
     pwreset(context, userEmailData) {
       axios
         .put(
