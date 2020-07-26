0bf2e4558257baaacedf2915b446667ebc7bad1f
YongjoonSeo <koreakkrea12@naver.com>
2020-07-26 19:36:56 +0900

    Feat user editing page & Modify banner logo

---
+++ frontend/src/components/accounts/useredit/UserEdit.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-col cols="12" sm="8" md="6" lg="4">
+    <h2 class="text-left red--text text--lighten-2 ml-3">회원정보 수정</h2>
+    <br />
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <v-btn large color="#2699fb" width="100%" dark>비밀번호 변경</v-btn>
+        </v-col>
+        <v-col cols="12">
+          <v-btn large color="#2699fb" width="100%" dark>이메일 변경</v-btn>
+        </v-col>
+        <v-col cols="12">
+          <v-btn large color="#2699fb" width="100%" dark>차단 목록 관리</v-btn>
+        </v-col>
+        <v-col cols="6" class="mt-10">
+          <v-btn large color="#2699fb" width="100%" dark>공개</v-btn>
+        </v-col>
+        <v-col cols="6" class="mt-10">
+          <v-btn large width="100%" disabled>비공개</v-btn>
+          <!-- 추후에 공개 계정인지 비공개 계정인지에 따라 조정되도록 변경 필요 -->
+        </v-col>
+        <v-col cols="12" class="mt-10">
+          <v-btn large color="#D50000" width="100%" dark>회원 탈퇴</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-col>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
