70f959d3469622db7caa8be7641f0bcc7d7281da
rsm4282@naver.com <rsm4282@naver.com>
2020-07-30 18:08:05 +0900

    Modify FeedItem.vue, Media.vue & Add comments.js

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,12 +4,14 @@ import createPersistedState from "vuex-persistedstate";
 
 import moduleAccounts from "./accounts";
 import moduleFeeds from "./feeds";
+import moduleComments from "./comments";
 
 Vue.use(Vuex);
 
 const modules = {
   accounts: moduleAccounts,
   feeds: moduleFeeds,
+  comments: moduleComments,
 };
 
 export default new Vuex.Store({
