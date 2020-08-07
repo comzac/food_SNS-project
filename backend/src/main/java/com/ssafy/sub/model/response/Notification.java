0f3604b3c693281e25934303cab146d0a3e890bf
Sooni <ksb940925@gmail.com>
2020-08-07 10:31:25 +0900

    Modify notification response and insert | S03P13A108-8

--- backend/src/main/java/com/ssafy/sub/model/response/Notification.java
+++ backend/src/main/java/com/ssafy/sub/model/response/Notification.java
@@ -1,5 +1,7 @@
 package com.ssafy.sub.model.response;
 
+import com.ssafy.sub.dto.DBProfile;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,10 +16,13 @@ import lombok.Setter;
 public class Notification {
 	private int id;
 	private int state;
-	private String uid;
+	private String uid;	// 알림 받는 사람의 아이디
+	private String unick;	// 알림 받는 사람의 닉네임
 	private int fid;
 	private String title;
 	private String followid;
 	private String likeid;
 	private String commentid;
+	private String notiUnick;	// 팔로우, 좋아요, 댓글을 단 사람의 닉네임
+	private DBProfile notiUprofile; // 팔로우, 좋아요, 댓글을 단 사람의 프로필
 }
