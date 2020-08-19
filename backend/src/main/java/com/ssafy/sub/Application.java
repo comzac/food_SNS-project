b6d3b7ae598d1ee79fb39427a70a6ae9ee2a7310
이선수 <tjstndi77@naver.com>
2020-08-19 20:30:41 +0900

    Refactor SpringBootApplication rename | S03P13A108-168 Actions (Type . to access issue actions)

--- backend/src/main/java/com/ssafy/sub/Application.java
+++ backend/src/main/java/com/ssafy/sub/Application.java
@@ -6,10 +6,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class Test1Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Test1Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- backend/src/main/java/com/ssafy/sub/ServletInitializer.java
+++ backend/src/main/java/com/ssafy/sub/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Test1Application.class);
+		return application.sources(Application.class);
 	}
 
 }
